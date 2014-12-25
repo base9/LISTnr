fc171df2472a0b2fd75174c68fc30249039ff45b
Brian Scoles <brianscoles@gmail.com>
2014-12-24 20:54:38 -0800

    expand logic in message parsing

--- hello/views.py
+++ hello/views.py
@@ -5,8 +5,31 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    result = 2+2
-    return HttpResponse(result)
+    message = request.GET.get('message', '')
+
+    freq = {}
+    nWords = 0
+    for word in message.split():
+      nWords += 1
+      if word in freq:
+        freq[word] += 1
+      else:
+        freq[word] = 1
+         
+    eventwords = 'event rsvp public pm am p.m. a.m. gather meet \
+      party picnic rally parade barbecue festival happening get-together luncheon'
+
+    probability = 0
+
+    for word in eventwords.split():
+      if word in freq:
+        probability += 20*freq[word]
+
+
+    print "I am {0} percent certain that this text describes an event.".format(probability)
+
+
+    return HttpResponse(probability)
 
 
 def db(request):
